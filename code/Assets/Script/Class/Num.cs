0c795bed9207362747704f0fcaf7921564462f9e
snow <leebaojie@sina.com>
2015-01-29 21:19:23 +0800

    添加了Class 并实验成功！

---
+++ code/Assets/Script/Class/Num.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Num
+{
+	public void print()
+	{
+		Debug.Log ("11111111");
+	}
+
+}
---
+++ code/Assets/Script/Controller/Demo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Num num = new Num ();
+		num.print ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
