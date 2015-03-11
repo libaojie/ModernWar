a75509c499a756153614b0f955cfe93b832d6b96
snow <leebaojie@sina.com>
2015-03-11 20:29:36 +0800

    加入了loading场景

---
+++ code/Assets/Script/Others/Loading.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// 场景加载
+/// </summary>
+public class Loading : MonoBehaviour {
+	
+	/// <summary>
+	/// 异步加载操作
+	/// </summary>
+	private AsyncOperation async;
+
+	/// <summary>
+	/// 进度条UI
+	/// </summary>
+	private UISlider uiSlider = null;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		uiSlider = this.gameObject.GetComponent<UISlider>();
+
+		StartCoroutine(loadingAsync());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// 更新加载进度
+		uiSlider.value = async.progress;	
+	}
+
+	/// <summary>
+	/// 加载的异步函数
+	/// </summary>
+	/// <returns>The async.</returns>
+	private IEnumerator loadingAsync()
+	{
+		async = Application.LoadLevelAsync(Global.ScenceName);
+
+		yield return async;
+	}
+
+}
---
+++ code/Assets/Script/Tool/Global.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 全局变量
+/// </summary>
+public class Global{
+
+	/// <summary>
+	/// 当前Scence名
+	/// </summary>
+		public static string ScenceName = "Hawaii";
+
+
+}
