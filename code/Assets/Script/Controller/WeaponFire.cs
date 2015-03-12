c27142b8ead4bf01500b7224d94ff6366f548253
snow <leebaojie@sina.com>
2015-03-12 20:45:10 +0800

    加入了  导弹  武器

---
+++ code/Assets/Script/Controller/WeaponFire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponFire : MonoBehaviour {
+
+	public GameObject Weapon;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick()
+	{			
+		Weapon.GetComponent<WeaponLauncher>().Shoot();
+	}
+}
