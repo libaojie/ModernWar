b50dfd830aaf77687836faa11611cd47d7d073ba
snow <leebaojie@sina.com>
2015-03-13 01:19:11 +0800

    加入了  所有武器   并切换

--- code/Assets/Script/Controller/WeaponFire.cs
+++ code/Assets/Script/Controller/WeaponFire.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
+/// <summary>
+/// 开火
+/// </summary>
 public class WeaponFire : MonoBehaviour {
 
+	/// <summary>
+	/// 武器类
+	/// </summary>
 	public GameObject Weapon;
-	
-	
+
+	/// <summary>
+	/// 武器组件
+	/// </summary>
+	private WeaponLauncher weaponLauncher;
+
+	/// <summary>
+	/// 是否长按
+	/// </summary>
+	private Boolean isPressed = false;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		weaponLauncher = Weapon.GetComponent<WeaponLauncher>();
 	}
 	
 	// Update is called once per frame
@@ -16,8 +33,34 @@ public class WeaponFire : MonoBehaviour {
 	
 	}
 
+
+	void OnPress(bool isPressed)
+	{
+//		Debug.Log("OnPress:"+isPressed);
+		this.isPressed = isPressed;
+
+		if (isPressed)
+		{
+			StartCoroutine(fireAsync());
+		}
+
+	}
+
+	private IEnumerator fireAsync()
+	{
+
+		while (this.isPressed)
+		{
+			yield return new WaitForSeconds(0.1f);
+			weaponLauncher.Shoot();
+		}
+
+	}
+
+	
+
 	void OnClick()
 	{			
-		Weapon.GetComponent<WeaponLauncher>().Shoot();
+		//weaponLauncher.Shoot();
 	}
 }
